62d4580d67c1e7b7983c2f09b0da33604a441cd9
Marcel Widmer <marzelwidmer@gmail.com>
2019-03-25 21:30:29 +0100

    WIP soap routes

--- src/main/kotlin/ch/keepcalm/example/camel/CamelApplication.kt
+++ src/main/kotlin/ch/keepcalm/example/camel/CamelApplication.kt
@@ -12,25 +12,47 @@ import org.springframework.stereotype.Component
 class CamelApplication
 
 fun main(args: Array<String>) {
-	runApplication<CamelApplication>(*args)
+    runApplication<CamelApplication>(*args)
 }
 
 
 @RestController
 class CamelController(val producerTemplate: ProducerTemplate) {
 
-	@RequestMapping(value = ["/"])
-	fun startCamel() {
-		producerTemplate!!.sendBody("direct:firstRoute", "Calling via Spring Boot Rest Controller")
-	}
+    @RequestMapping(value = ["/"])
+    fun startCamel() {
+        producerTemplate.sendBody("direct:firstRoute", "Calling via Spring Boot Rest Controller")
+    }
+
+    @RequestMapping(value = ["/holiday"])
+    fun startHolliday() {
+        producerTemplate.sendBody("direct:holidayService",
+                " <hs:GetHolidaysAvailable xmlns:hs=\"http://www.holidaywebservice.com/HolidayService_v2/\">\n" +
+                        " <hs:countryCode>UnitedStates</hs:countryCode>\n" +
+                        " </hs:GetHolidaysAvailable>"
+        )
+    }
 }
 
 
 @Component
 class CamelRoutes : RouteBuilder() {
-	@Throws(Exception::class)
-	override fun configure() {
-		from("direct:firstRoute")
-				.log("Camel body: \${body}")
-	}
+    @Throws(Exception::class)
+    override fun configure() {
+        from("direct:firstRoute")
+                .log("Camel body: \${body}")
+    }
 }
+
+@Component
+class HolidayRoutes : RouteBuilder() {
+    @Throws(Exception::class)
+    override fun configure() {
+        from("direct:holidayService")
+                .setHeader("operationName", constant("GetCustomer"))
+                .to("spring-ws:http://www.holidaywebservice.com//HolidayService_v2/HolidayService2.asmx")
+        //                .to("spring-ws:http://www.holidaywebservice.com//HolidayService_v2/HolidayService2.asmx?soapAction=http://foo.com&wsAddressingAction=http://bar.com")
+
+    }
+}
+
